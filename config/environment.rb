8c7e217d2b36e43b4e7ca4540333bf3badecf804
Jake <github@jake.otherinbox.com>
2010-08-29 18:28:26 -0700

    Changed to use markaby gem.

--- config/environment.rb
+++ config/environment.rb
@@ -8,6 +8,7 @@ ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 #RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,11 +20,17 @@ Rails::Initializer.run do |config|
 
 	config.active_record.observers = :user_observer
 
+	config.gem 'hpricot'	#	for ripper
+
 	config.gem 'mysql'
 
-	config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
+	config.gem 'will_paginate', 
+		:version => '~> 2.3.11', 
+		:source => 'http://gemcutter.org'
 
 	config.gem 'gravatar'
+
+	config.gem 'markaby'
 
 	config.gem 'jakewendt-simply_commentable',
 		:lib    => 'simply_commentable', 

0444355c7ef0eee974aec8a0a7d168f12480c970
Jake <github@jakewendt.com>
2013-01-29 16:31:40 -0800

    Downgraded mysql version

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 #RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+#RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -22,7 +22,8 @@ Rails::Initializer.run do |config|
 
 	config.gem 'hpricot'	#	for ripper
 
-	config.gem 'mysql'
+	#	seems somethings wrong that causes "Virtual timer expired" with newer
+	config.gem 'mysql', :version => '2.7'
 
 	config.gem 'will_paginate', 
 		:version => '~> 2.3', 
